880c566d25b0881e8642a1a5c2e319f9440ef020
ChrisOmni <chris.duca@jobtrek.ch>
2025-03-05 08:05:38 +0100

    finished the fetchDataOnClick function in fetchData.js. finished everything

--- src/fetch/fetchData.js
+++ src/fetch/fetchData.js
@@ -6,5 +6,21 @@
  * Then, you have to display the content returned by the api on the "pre" tag with id "display-here"
  */
 export function fetchDataOnClick() {
-  // Write your code here
+  //  Write your code here 
+  const resultPlace = document.getElementById("display-here")
+  const fetchBtn = document.getElementById("click-to-fetch")
+  fetchBtn.addEventListener("click", () => {
+    fetch("https://api.github.com/octocat", {
+      method: "GET",
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Response Status : ${response.status}`)
+        }
+        return response.text()
+      })
+      .then((text) => {
+        resultPlace.innerText = text
+      })
+  })
 }
